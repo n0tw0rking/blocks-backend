1702bd8c9f889c0747bd8bc3b93c3066558cf36d
weeehabla <weeehaplay@gmail.com>
2020-02-12 19:34:40 +0100

    push notification done

--- src/server.js
+++ src/server.js
@@ -4,33 +4,32 @@ const graphql = require("./loaders/graphql");
 const sms = require("./loaders/sms");
 const email = require("./loaders/mailer");
 const image = require("./loaders/imagesUploader");
-const bodyParser = require("body-parser");
 const notpush = require("./loaders/web_push");
 const cors = require("cors");
 
 async function startServer() {
-  const app = express();
-  app.use(cors());
+    const app = express();
+    app.use(cors());
 
-  sms(app);
-  email(app);
-  image(app);
-  notpush(app);
-  graphql(app);
+    sms(app);
+    email(app);
+    image(app);
+    notpush(app);
+    graphql(app);
 
-  await require("./loaders/mongoose").mongooseConnect();
-  app.listen(config.port, err => {
-    if (err) {
-      console.Error(err);
-      process.exit(1);
-      return;
-    }
-    console.log(`
+    await require("./loaders/mongoose").mongooseConnect();
+    app.listen(config.port, (err) => {
+        if (err) {
+            console.Error(err);
+            process.exit(1);
+            return;
+        }
+        console.log(`
       ################################################
            🛡️  Server listening on port: ${config.port} 🛡️ 
       ################################################
     `);
-  });
+    });
 }
 
 startServer();
