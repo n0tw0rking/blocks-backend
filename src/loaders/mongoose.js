cf0c946875446ec9d95df98aeec5b2e38fbe40c1
AdamMomen <adammuman81@gmail.com>
2020-02-15 11:03:58 +0100

    (deploy) deploying to Heroku

--- src/loaders/mongoose.js
+++ src/loaders/mongoose.js
@@ -1,9 +1,22 @@
 const mongoose = require("mongoose");
 const { config } = require("../config/index");
 
+const mongooseConnect = async () => {
+  await mongoose
+    .connect(config.databaseURL, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true
+    })
+    .then(() => {
+      console.log("✌️ DB loaded and connected!");
+    });
+};
 // const mongooseConnect = async () => {
 //     await mongoose
-//         .connect(config.databaseURL, {
+//         .connect(
+//             "mongodb://localhost:27017/appEvent"
+//             , {
 //             useNewUrlParser: true,
 //             useCreateIndex: true,
 //             useUnifiedTopology: true
@@ -12,18 +25,7 @@ const { config } = require("../config/index");
 //             console.log("✌️ DB loaded and connected!");
 //         });
 // };
-const mongooseConnect = async () => {
-    await mongoose
-        .connect("mongodb://localhost:27017/appEvent", {
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useUnifiedTopology: true
-        })
-        .then(() => {
-            console.log("✌️ DB loaded and connected!");
-        });
-};
 
 module.exports = {
-    mongooseConnect
+  mongooseConnect
 };
